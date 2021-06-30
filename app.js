d7715dd7bc14ad4b2526327415797fb4afb16f3e
guillermoe3 <guillermojesq@gmail.com>
2021-06-30 18:07:51 -0300

    heroku prep

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const app = express();
 const path = require("path");
 
 
-
+/*
 app.listen(3002, function(){
     return console.log("El servidor se encuentra levantado")
+})*/
+
+app.listen(process.env.PORT || 3000, function(){
+    return console.log("El servidor se encuentra corriendo en el puerto 3000")
 })
 
 //public access
